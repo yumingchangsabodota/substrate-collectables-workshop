b804d1a715a66630674938d451c629f4efdd9674
yu-ming <z1e2r3o4o5n6e7@gmail.com>
2025-08-19 00:01:08 +0800

    sotring objects - bounded vectors

--- src/impls.rs
+++ src/impls.rs
@@ -21,7 +21,7 @@ impl<T: Config> Pallet<T> {
 		ensure!(!Kitties::<T>::contains_key(dna), Error::<T>::DuplicateKitty);
 		let current_count: u32 = CountForKitties::<T>::get();
 		let new_count = current_count.checked_add(1).ok_or(Error::<T>::TooManyKitties)?;
-		KittiesOwned::<T>::append(&owner, dna);
+		KittiesOwned::<T>::try_append(&owner, dna).map_err(|_| Error::<T>::TooManyOwned)?;
 		Kitties::<T>::insert(dna, kitty);
 		CountForKitties::<T>::set(new_count);
 		Self::deposit_event(Event::<T>::Created { owner });
--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@ pub mod pallet {
 	pub(super) type Kitties<T: Config> = StorageMap<Key = [u8; 32], Value= Kitty<T>>;
 
 	#[pallet::storage]
-	pub(super) type KittiesOwned<T: Config> = StorageMap<Key = T::AccountId, Value= Vec<[u8; 32]>, QueryKind = ValueQuery>;
+	pub(super) type KittiesOwned<T: Config> = StorageMap<Key = T::AccountId, Value= BoundedVec<[u8; 32], ConstU32<100>>, QueryKind = ValueQuery>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -44,6 +44,7 @@ pub mod pallet {
 	pub enum Error<T> {
 		TooManyKitties,
 		DuplicateKitty,
+		TooManyOwned,
 	}
 
 	#[pallet::call]
