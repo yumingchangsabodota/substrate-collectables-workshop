24e039c51ca31ca47e52b1c95c45077e4fe7aa62
yu-ming <z1e2r3o4o5n6e7@gmail.com>
2025-08-18 23:49:30 +0800

    sotring objects - track owned kitties

--- src/impls.rs
+++ src/impls.rs
@@ -21,6 +21,7 @@ impl<T: Config> Pallet<T> {
 		ensure!(!Kitties::<T>::contains_key(dna), Error::<T>::DuplicateKitty);
 		let current_count: u32 = CountForKitties::<T>::get();
 		let new_count = current_count.checked_add(1).ok_or(Error::<T>::TooManyKitties)?;
+		KittiesOwned::<T>::append(&owner, dna);
 		Kitties::<T>::insert(dna, kitty);
 		CountForKitties::<T>::set(new_count);
 		Self::deposit_event(Event::<T>::Created { owner });
